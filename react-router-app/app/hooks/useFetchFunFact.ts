d454fd8af70513a58a0e63722b98666b80bcf896
luisarevalo21 <arevaloluis89@yahoo.com>
2026-02-11 10:41:47 -0800

    added changes

--- react-router-app/app/hooks/useFetchFunFact.ts
+++ react-router-app/app/hooks/useFetchFunFact.ts
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export type FunFactHookType = [
-    string, 
-    React.Dispatch<React.SetStateAction<string>>, 
-    ()=>void
-];
+//no need to type the setFunFact
+export type FunFactHookType = [string, () => void];
 
 export default function useFetchFunFact(): FunFactHookType {
-    const [funFact, setFunFact] = useState<string>('');
-    
-    const getDogFact = async () => {
-        try {
-            const response = await fetch("https://dogapi.dog/api/v2/facts?limit=1");
-            if (!response.ok) throw new Error('Error occured');
-            const JSONResponse = await response.json();
-            setFunFact(JSONResponse.data[0].attributes.body)
-        }
-        catch(error) {
-            console.error(error);
-        }
+  const [funFact, setFunFact] = useState<string>("");
+
+  const getDogFact = async () => {
+    try {
+      const response = await fetch("https://dogapi.dog/api/v2/facts?limit=1");
+      if (!response.ok) throw new Error("Error occured");
+      const JSONResponse = await response.json();
+      setFunFact(JSONResponse.data[0].attributes.body);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    useEffect(() => {getDogFact()}, []);
+  //great decision to outsource the getDog Func so you can reuse it on button click
+  useEffect(() => {
+    getDogFact();
+  }, []);
 
-    return [funFact, setFunFact, getDogFact];
+  //no need to export the setFunfact as gotDogFact is using it interally to update the state for the instant
+  return [funFact, getDogFact];
 }
--- react-router-app/app/routes/custom-hook-fetch.tsx
+++ react-router-app/app/routes/custom-hook-fetch.tsx
@@ -1,45 +1,22 @@
-import useFetchFunFact, { type FunFactHookType } from "~/hooks/useFetchFunFact";
+import useFetchFunFact from "~/hooks/useFetchFunFact";
 
 export default function CustomHookFetchRoute() {
-    const [
-        funFact, 
-        setFunFact, 
-        getDogFact
-    ]: FunFactHookType = useFetchFunFact();
-    
-    async function handleClick() {
-        await getDogFact();
-    }
+  //no need to type it since useFetchFunFact is type it is inferred here
+  const [funFact, getDogFact] = useFetchFunFact();
 
-    return (
-        <div
-            className="
-                flex flex-col
-                items-center
-                gap-4
-                m-4
-                p-4
-                h-[calc(100vh-2rem)]
-                bg-gray-700
-            "
-        >
-            <button 
-                className="
-                    bg-white 
-                    text-black 
-                    py-1 
-                    px-2.5
-                    rounded-md
-                    hover:bg-gray-300 
-                    transition-colors 
-                "
-                onClick={handleClick}
-            >
-                Get New Dog Fact
-            </button>
-            <br />
-            <p>Dog fact:</p>
-            <p className="text-center">{funFact}</p>
-        </div>
-  )
+  // doesn't need to be async as getDogFact is async already and will wait there.
+  function handleClick() {
+    getDogFact();
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-4 m-4 p-4 h-[calc(100vh-2rem)]bg-gray-700">
+      <button className="bg-white text-black py-1 px-2.5 rounded-md hover:bg-gray-300 transition-colors" onClick={handleClick}>
+        Get New Dog Fact
+      </button>
+      <br />
+      <p>Dog fact:</p>
+      <p className="text-center">{funFact}</p>
+    </div>
+  );
 }
